dbf2130e3be1a03dd824681264b24bb25ff576b7
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-05-27 00:54:30 +0800

    change PostView

---
+++ frontend/src/components/Post/PostPage.vue
@@ -0,0 +1,89 @@
+<template>
+  <PostItem 
+        v-for="(item, i) in tripProps.photos_src.length"
+        :key="i"
+        :picture="tripProps.photos_src[i]"
+        :attr_name="tripProps.photos_title[i]"
+        :text="tripProps.texts[i]"
+    />
+</template>
+
+<script>
+import PostItem from './PostItem.vue'
+
+  export default {
+    name: 'PostPage',
+    components: {
+        PostItem,
+    },
+    data(){
+        return {
+        //   items: [
+        //       {
+        //           pic: "https://www.treksplorer.com/wp-content/uploads/things-to-do-in-taipei-taiwan-1-1024x683.jpg",
+        //           name: "xxx temple",
+        //           text: [
+        //               "123Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //               "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //           ]
+
+        //       },
+        //       {
+        //           pic: "https://www.treksplorer.com/wp-content/uploads/ximending-taipei-taiwan-night.jpg",
+        //           name: "Ximending",
+        //           text: [
+        //               "456Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //               "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //               "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //               "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //           ]
+
+        //       },
+        //       {
+        //           pic: "https://images.chinatimes.com/newsphoto/2022-01-19/1024/20220119001717.jpg",
+        //           name: "NTU Park",
+        //           text: [
+        //               "789Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //               "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+        //           ]
+        //       },
+        //   ],
+      }
+    },
+    props: {
+      'tripProps': {
+        type: Object,
+        default(){
+          return {
+            
+          }
+        }
+      }
+    },
+    methods: {
+      
+    },
+    mounted() {
+      
+    }
+  }
+</script>
+
+<style>
+.name{
+    font-weight: 900;
+}
+.trip_name{
+    color: rgb(107, 107, 107);
+    font-weight: 500;
+
+}
+.intro{
+    max-width: 650px;
+    margin-top: 30px; 
+    margin-bottom: 30px;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: left;
+}
+</style>
--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -2,10 +2,47 @@
   <div>
     <v-container class="pa-0">
       <v-col align="center" class="pa-0">
-        <PostCard 
-            class="my-2"
-            :tripProps="trip"    
-        />
+        <v-card
+            class = "pa-3 .bg-white rounded-xl flex-column flex-grow mt-1"
+            elevation = "4"
+            max-height = "100%"
+            max-width = "750px"
+            min-width = "300px"
+        >
+            <v-row 
+                class = "mt-3 ml-2"
+                align = "center"
+            >
+                <v-avatar
+                    size="72"
+                >
+                    <v-img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="avatar"
+                    ></v-img>
+                </v-avatar>
+                <v-col
+                    align = "start"
+                >
+                    <p class = "name"> {{ post.creator_name }}</p>
+                    <p class = "trip_name"> {{ post.trip_title }}</p>
+                </v-col>
+            </v-row>
+            <TripCard 
+                class = "mt-8"
+                :tripProps="trip"
+            />
+            <p class = "intro">{{ post.foreword }}</p> 
+            <PostPage
+                class="my-2"
+                :tripProps="post"    
+            />  
+        </v-card>
+        
+        <BottomBar
+            :author = "post.creator_name"
+            date = "2023-08-19"
+         />
       </v-col>
     </v-container>
     
@@ -13,17 +50,19 @@
 </template>
 
 <script>
-import PostCard from '@/components/Post/PostCard.vue'
+import TripCard from '@/components/Home/TripCard.vue'
+import PostPage from '@/components/Post/PostPage.vue'
+import BottomBar from '@/components/Post/BottomBar.vue'
 
 export default {
   name: 'PostView',
   components: {
-    PostCard,
+    TripCard,
+    PostPage,
+    BottomBar,
   },
   data() {
       return {
-        drawer: false,
-        group: null,
         trip: {
             id: 0,
             invited: false,
@@ -34,6 +73,42 @@ export default {
             startDate: "2023/08/12",
             endDate: "2023/08/15",
             area: "Taipei",
+        },
+        post: {
+            id: 0,
+            creator: 102,
+            trip: 51022,
+            content: [],
+            creator_name: "derekdylu",
+            trip_title: "taipei trip",
+            picture: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+            foreword: "This is the brief introduction of this travel post... post... post... . . . . post... post... post... post...",
+            photos_src: [
+                "https://www.treksplorer.com/wp-content/uploads/things-to-do-in-taipei-taiwan-1-1024x683.jpg",
+                "https://www.treksplorer.com/wp-content/uploads/ximending-taipei-taiwan-night.jpg",
+                "https://images.chinatimes.com/newsphoto/2022-01-19/1024/20220119001717.jpg",
+            ],
+            photos_title: [
+                "xxx temple",
+                "Ximending",
+                "NTU Park",
+            ],
+            texts: [
+                [
+                    "123Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                    "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                ],
+                [
+                    "456Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                    "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                    "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                    "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                ],
+                [
+                    "789Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                    "next Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, urna non sodales aliquam, est ex tempus nunc, a porttitor nisl erat congue mi.",
+                ],
+            ],
         },
       }
     },
