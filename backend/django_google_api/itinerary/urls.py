c981d9ef69ea60b5cfafe84fe0df3d51e63eec12
brandon9912 <b08705028@ntu.edu.tw>
2022-05-27 02:39:38 +0800

    update backend

--- backend/django_google_api/itinerary/urls.py
+++ backend/django_google_api/itinerary/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    #.as_view only required when using class-based view
+	#.as_view only required when using class-based view
 	path('itinerary/', views.ItineraryView.as_view(), name="Itinerary_View"),
-	]
+]
