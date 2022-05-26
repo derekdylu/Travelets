d6301d7a8ffb9183eb58603ba30ceb7bc7d93b8c
brandon9912 <b08705028@ntu.edu.tw>
2022-05-26 21:13:41 +0800

    added itinerary model fields & update README

--- backend/django_google_api/itinerary/models.py
+++ backend/django_google_api/itinerary/models.py
@@ -1,10 +1,17 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Itinerary(models.Model):
-    
-    title = models.CharField(verbose_name="Title",max_length=100, null=True, blank=True)
+    id = models.UUIDFeild(primary_key=True, default=uuid.uuid4, editable=False)
+    trip_name = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    vehicle = models.CharField(max_length=100, null=True, blank=True)
     start_date = models.DateField(auto_now_add=False, null=True, blank=True)
+    end_date = models.DateField(auto_now=False, null=True, blank=True)
+    attractions = models.JSONField(null=True, blank=True, editable=True)
+    travel_time = models.JSONField(null=True, blank=True, editable=True)
+    notes = models.JSONField(null=True, blank=True, editable=True)
     
     def __str__(self):
         return f'{self.title}'
