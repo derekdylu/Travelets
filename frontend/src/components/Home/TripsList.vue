f19a465f30b2a67810dc13bd53c5c76e4eb76b4f
derekdylu <derekdylu@gmail.com>
2022-05-27 00:03:04 +0800

    d

--- frontend/src/components/Home/TripsList.vue
+++ frontend/src/components/Home/TripsList.vue
@@ -1,33 +1,80 @@
 <template>
-  <v-container fluid class="flex-column flex-grow" style="width: 750px">
-    <v-row align="center" justify="space-between" class="mx-4">
-      <v-btn-toggle>
-        <v-icon icon="more_verti" color="secondary" class="my-3" />
-      </v-btn-toggle>
-      <v-btn variant="outlined" color="primary" @click="this.$router.push({ path: '/setup' })">
-        new trip
-      </v-btn>
-    </v-row>
-    <v-col align="center">
-      <TripCard
-        class="my-2"
-        v-for="(trip, i) in trips"
-        :key="i"
-        :tripProps="trip"
-      />
-    </v-col>
-  </v-container>
+
+  <div>
+    <v-tabs
+      v-model="tab"
+      center-active
+      grow
+      color="primary"
+      class="mb-1"
+      style="max-width: 750px"
+    >
+      <v-tab value="trip">trip</v-tab>
+      <v-tab value="post">post</v-tab>
+      <v-tab value="saved">saved</v-tab>
+    </v-tabs>
+
+      <v-window v-model="tab">
+        <v-window-item value="trip">
+          <v-container fluid class="flex-column flex-grow" style="width: 750px">
+            <v-row align="center" justify="space-between" class="mx-4">
+              <v-btn-toggle>
+                <v-icon icon="more_verti" color="secondary" class="my-3" />
+              </v-btn-toggle>
+              <v-btn variant="outlined" color="primary" @click="this.$router.push({ path: '/setup' })">
+                new trip
+              </v-btn>
+            </v-row>
+            <v-col align="center">
+              <TripCard
+                class="my-2"
+                v-for="(trip, i) in trips"
+                :key="i"
+                :tripProps="trip"
+              />
+            </v-col>
+          </v-container>
+        </v-window-item>
+      </v-window>
+
+      <v-window v-model="tab">
+        <v-window-item value="post">
+          <v-container fluid class="flex-column flex-grow" style="width: 750px">
+            <v-row align="center" justify="space-between" class="mx-4">
+              <v-btn-toggle>
+                <v-icon icon="more_verti" color="secondary" class="my-3" />
+              </v-btn-toggle>
+              <v-btn variant="outlined" color="primary" @click="this.$router.push({ path: '/setup' })">
+                new trip
+              </v-btn>
+            </v-row>
+            <v-col align="center">
+              <PostCard
+                class="my-2"
+                v-for="(trip, i) in trips"
+                :key="i"
+                :tripProps="trip"
+              />
+            </v-col>
+          </v-container>
+        </v-window-item>
+      </v-window>
+  </div>
+  
 </template>
 
 <script>
   import TripCard from './TripCard.vue'
+  import PostCard from '../Post/PostCard.vue'
 
   export default {
     components: {
-      TripCard
+      TripCard,
+      PostCard
     },
     data() {
       return {
+        tab: null,
         trips: [
           {
             id: 0,
