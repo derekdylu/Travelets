7d65ac3aa5303dfcaa553f70b83d7761db309816
brandon9912 <b08705028@ntu.edu.tw>
2022-05-28 17:49:49 +0800

    API update

--- backend/django_google_api/itinerary/views.py
+++ backend/django_google_api/itinerary/views.py
@@ -1,4 +1,3 @@
-import imp
 from django.shortcuts import render
 
 from rest_framework.response import Response
@@ -21,15 +20,15 @@ class ItineraryViewSet(ModelViewSet):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
 
-    def create(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            tripname = request.POST.get('tripname')
-            startdate = request.POST.get('startdate')
-            enddate = request.POST.get('enddate')
-            location = request.POST.get('location')
-            vehicle = request.POST.get('vehicle')
-            newTrip = Itinerary.objects.create(tripname=tripname, location=location,vehicle=vehicle, startDate=startdate, endDate=enddate)
-            newTrip.save()
+    # def create(self, request, *args, **kwargs):
+    #     if request.method == 'POST':
+    #         tripname = request.POST.get('tripname')
+    #         startdate = request.POST.get('startdate')
+    #         enddate = request.POST.get('enddate')
+    #         location = request.POST.get('location')
+    #         vehicle = request.POST.get('vehicle')
+    #         newTrip = Itinerary.objects.create(tripname=tripname, location=location,vehicle=vehicle, startDate=startdate, endDate=enddate)
+    #         newTrip.save()
 
-            serializer = ItinerarySerializer(newTrip)
-            return Response(serializer.data["id"])
+    #         serializer = ItinerarySerializer(newTrip)
+    #         return Response(serializer.data)
