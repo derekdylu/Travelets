a916c002c27574cf90b6508d99ba183ce34e68c4
derekdylu <derekdylu@gmail.com>
2022-05-28 18:16:10 +0800

    ap

--- frontend/src/views/SetupView.vue
+++ frontend/src/views/SetupView.vue
@@ -63,8 +63,8 @@ export default {
 
       await axios.post('http://127.0.0.1:8000/itinerary/', formdata)
         .then(response => {
-          console.log(response.data)
-          this.$store.dispatch('updateId', response.data)
+          console.log(response.data.id)
+          this.$store.dispatch('updateId', response.data.id)
         })
         .catch(error => {
           console.log(error)
