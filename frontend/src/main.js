3f350c20939c90e8b3ba19efe0276931f50d6a4b
derekdylu <derekdylu@gmail.com>
2022-05-29 04:57:43 +0800

    mock additems

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,15 @@ import router from './router/index'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import store from './store/index'
+// import axios from 'axios'
+
+var cors = require('cors');
 
 createApp(App)
   .use(router)
   .use(vuetify)
   .use(store)
+  .use(cors)
   .mount('#app')
 
 loadFonts()
