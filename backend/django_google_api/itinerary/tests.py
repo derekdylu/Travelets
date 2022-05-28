c5484e610a491adab53a414932688bbff60f53e9
jenna <b07610008@ntu.edu.tw>
2022-05-29 03:09:04 +0800

    fix status bug

--- backend/django_google_api/itinerary/tests.py
+++ backend/django_google_api/itinerary/tests.py
@@ -10,9 +10,15 @@ from .models import Itinerary
 class TestItinerary(TestCase):
     def setUp(self):
         Itinerary.objects.create(tripname = 'Green Island Trip', startDate = '2022-02-28', endDate = '2022-03-05', host = 'somebody')
-        # Itinerary.objects.create()
+        Itinerary.objects.create(tripname = 'Go to Japan', startDate = '2022-05-31', endDate = '2022-06-15', host = 'monkey', vehicle = 'boat')
 
-    def test_attribute(self):
+    def test_host(self):
         greentrip = Itinerary.objects.get(tripname = 'Green Island Trip')
         self.assertEqual(greentrip.host, 'somebody')
+
+    def test_status(self):
+        greentrip = Itinerary.objects.get(tripname = 'Green Island Trip')
+        jptrip = Itinerary.objects.get(tripname = 'Go to Japan')
+        self.assertEqual(greentrip.status, 'past')
+        self.assertEqual(jptrip.status, 'ongoing')
 
