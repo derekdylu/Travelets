dba7829ab1d89dcbd5c012a40547a7d88a4e6be3
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-05-29 20:19:29 +0800

    add more tests, PopupEdit.spec.js & SetTrip.spec.js are with error

--- frontend/tests/unit/PopupEdit.spec.js
+++ frontend/tests/unit/PopupEdit.spec.js
@@ -1,12 +1,43 @@
 import { mount } from '@vue/test-utils'
 import PopupEdit from '@/components/AddNotes/PopupEdit.vue'
 
-describe('PopupEdit.dialog', () => {
-  it('renders data.dialog when passed', () => {
-    const dialog = 'true'
+describe('PopupEdit.vue', () => {
+  it('test click button',  async () => {
+    const $store = {
+      dispatch: jest.fn()
+    }
+
     const wrapper = mount(PopupEdit, {
-      props: { dialog }
+      global: {
+        mocks: {
+          $store: {
+            state: {
+              trip: {
+                id: "",
+                invited: false, 
+                picture: "",
+                tripname: "",
+                host: "",
+                location: "",
+                startDate: "",
+                endDate: "",
+                duration: 0,
+                vehicle: "car",
+                status: "",
+                attractions: [],
+                travelTimes: [],
+                notes: [],
+              },
+            } 
+          }
+        }
+      }
     })
-    expect(wrapper.text()).toMatch(dialog)
+
+    await wrapper.find('v-btn').trigger('click')
+
+    expect($store.dispatch).toHaveBeenCalledWith('click')
+
   })
+  
 })
---
+++ frontend/tests/unit/ProgressBar.spec.js
@@ -0,0 +1,42 @@
+import { shallowMount } from '@vue/test-utils'
+import ProgressBar from '@/components/General/ProgressBar.vue'
+import sinon from 'sinon'
+
+describe('ProgressBar.vue', () => {
+
+  it('show text of button in progress bar', () => {
+    const text = 'NEXT';
+    const wrapper = shallowMount(ProgressBar, {
+        propsData: {
+            primaryButton: 'NEXT'
+        }
+    });
+    expect(wrapper.text()).toMatch(text);
+``});
+
+  it('click on button to trigger event', async () => {
+    const wrapper = shallowMount(ProgressBar)
+
+    wrapper.vm.$emit('foo')
+    wrapper.vm.$emit('foo', 123)
+
+    await wrapper.vm.$nextTick() // Wait until $emits have been handled
+
+    /*
+    wrapper.emitted() returns the following object:
+    {
+        foo: [[], [123]]
+    }
+    */
+
+    // assert event has been emitted
+    expect(wrapper.emitted().foo).toBeTruthy()
+
+    // assert event count
+    expect(wrapper.emitted().foo.length).toBe(2)
+
+    // assert event payload
+    expect(wrapper.emitted().foo[1]).toEqual([123])
+  });
+
+});
